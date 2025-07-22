0728a37e2af3ec1b99150ca00736c78d0bdf5895
p3nc1l <szocsdavid598@gmail.com>
2025-07-22 17:32:31 +0300

    improved navbar pages logic and added mobile version of it

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,23 @@
-import { BottomNavigation, Box, Button } from '@mui/material'
+import { BottomNavigation, Box, Button, IconButton } from '@mui/material'
 import { BottomNavigationAction } from '@mui/material'
 import HomeFilled from '@mui/icons-material/HomeFilled';
 import FastfoodIcon from '@mui/icons-material/Fastfood';
 import { AccountCircle, AddBox, ListAlt } from '@mui/icons-material';
+import { isMobile } from 'react-device-detect';
 
 const Navbar = ({ page, setPage }: {page: number, setPage: (page: number) => void}) => {
   
 
   return (
-    <Box className='absolute bottom-2 left-1/2 -translate-x-1/2 border-gray-200 border-1 rounded-md shadow-md'>
-      <BottomNavigation className='rounded-md' showLabels value={page < 2 ? page : page + 1} onChange={(_, value) => setPage(value < 2 ? value : value - 1)}>
-        <BottomNavigationAction label="Home" icon={<HomeFilled />} />
-        <BottomNavigationAction label="Foods" icon={<FastfoodIcon />} />
-        <Button variant='contained' className='flex-col'><AddBox /><div>Add</div></Button>
-        <BottomNavigationAction label="Meals" icon={<ListAlt />} />
-        <BottomNavigationAction label="Profile" icon={<AccountCircle />} />
+    <Box className={`fixed border-gray-200 ${isMobile ? "bottom-0 w-screen border-t-1" : "bottom-2 left-1/2 -translate-x-1/2 border-gray-200 border-1 rounded-md shadow-md"}`}>
+      <BottomNavigation className='rounded-md' showLabels value={page} onChange={(_, value) => setPage(value)}>
+        <BottomNavigationAction label={!isMobile ? "Home" : ""} icon={<HomeFilled />} />
+        <BottomNavigationAction label={!isMobile ? "Foods" : ""} icon={<FastfoodIcon />} />
+        {!isMobile ?
+         <Button variant='contained' className='flex-col'><AddBox /><div>Add</div></Button> :
+         <IconButton size='large'><AddBox fontSize='large'/></IconButton> }
+        <BottomNavigationAction label={!isMobile ? "Meals" : ""} icon={<ListAlt />} value={2}/>
+        <BottomNavigationAction label={!isMobile ? "Profile" : ""} icon={<AccountCircle />} value={3}/>
       </BottomNavigation>
     </Box>
   )
