0dee7592a89a879fc601fbd50e8b8dfe22075b08
p3nc1l <szocsdavid598@gmail.com>
2025-07-23 23:10:31 +0300

    added searchbar and placeholder api call to foods page

--- src/components/pages/Foods.tsx
+++ src/components/pages/Foods.tsx
@@ -1,6 +1,41 @@
+import { Box, TextField } from "@mui/material";
+import { useEffect, useState } from "react";
+//import axios from "axios";
+
+const searchFoods = async (query: string) => {
+  try {
+    //fetch api and return response
+    return query || "";
+  } catch (error) {
+    console.error("Error fetching foods:", error);
+  }
+}
+
 const Foods = () => {
+  const [query, setQuery] = useState('');
+  const [results, setResults] = useState("");
+
+  useEffect(() => {
+    const fetchResults = async () => {
+      if (query.length > 2) {
+        const result = await searchFoods(query);
+        setResults(result || "");
+      } else {
+        setResults("");
+      }
+    };
+    fetchResults();
+  }, [query]);
+
   return (
-    <div className="w-screen flex-none" id="foods">Foods</div>
+    <Box className={"w-screen flex-none flex flex-col items-center"}>
+      <Box className="w-full max-w-7xl px-4 mb-16">
+        <Box className="mt-16">
+          <TextField fullWidth label="Search Foods" variant="outlined" value={query} onChange={(e) => setQuery(e.target.value)} />
+          {results}
+        </Box>
+      </Box>
+    </Box>
   )
 }
 
