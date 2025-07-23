c8c45479969d1df21794ebe5d228df2f1a41d6d6
p3nc1l <szocsdavid598@gmail.com>
2025-07-23 13:40:57 +0300

    added a nutrients today card to home page

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,8 +1,9 @@
-import { Box, Typography } from "@mui/material"
+import { Box, Grid, Typography } from "@mui/material"
 import Card from "@mui/material/Card"
 import CardContent from "@mui/material/CardContent"
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import { LocalDining, Whatshot } from "@mui/icons-material";
 
 const Home = () => {
   const dailyGoal = localStorage.getItem("dailyGoal");
@@ -10,16 +11,16 @@ const Home = () => {
 
   return (
     <Box className={"w-screen flex-none flex flex-col items-center"}>
-      <Box className="w-full max-w-7xl px-4">
+      <Box className="w-full max-w-7xl px-4 mb-16">
         <Box className="mt-20">
           <Typography sx={{ fontWeight: 600 }} variant="h3" component={"h1"}>Hi, <span className="bg-gradient-to-r from-green-700 to-yellow-500 text-transparent bg-clip-text">{localStorage.getItem("nickname")}</span><br />{(Number(dailyGoal) || 0) - reachedToday.calories} more calories to reach your daily goal</Typography>
         </Box>
-        <Box className="flex mt-20 gap-4">
+        <Box className="flex flex-row flex-wrap mt-20 gap-4">
           <Card>
             <CardContent>
               <Box className="flex flex-row items-start justify-center gap-12">
                 <Box className="flex flex-col gap-16">
-                  <Typography>Calories</Typography>
+                  <Box className="flex items-center justify-center w-max"><Whatshot color="primary" className="pb-0.5"/><Typography>Calories</Typography></Box>
                   <Typography variant="h5" component={"p"}>{reachedToday.calories || 0} / {dailyGoal || 0}</Typography>
                 </Box>
                 <Box className="w-32">
@@ -34,6 +35,16 @@ const Home = () => {
                     })} />
                 </Box>
               </Box>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardContent>
+              <Box className="flex items-center justify-center w-max"><LocalDining color="primary" className="pb-0.5"/><Typography>Nutrients Today</Typography></Box>
+              <Grid container spacing={2} className="mt-4">
+                <Grid size={10}><Typography>Fat</Typography></Grid><Grid size={2}><Typography>{reachedToday.fat || 0}g</Typography></Grid>
+                <Grid size={10}><Typography>Carbohydrates</Typography></Grid><Grid size={2}><Typography>{reachedToday.carbs || 0}g</Typography></Grid>
+                <Grid size={10}><Typography>Protein</Typography></Grid><Grid size={2}><Typography>{reachedToday.protein || 0}g</Typography></Grid>
+              </Grid>
             </CardContent>
           </Card>
         </Box>
