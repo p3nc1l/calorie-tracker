cbdc674b9940d1d264217b0886cdf05844b0cb3b
p3nc1l <szocsdavid598@gmail.com>
2025-07-24 10:38:35 +0300

    added placeholder data to home page

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -6,14 +6,14 @@ import 'react-circular-progressbar/dist/styles.css';
 import { LocalDining, Whatshot } from "@mui/icons-material";
 
 const Home = () => {
-  const dailyGoal = localStorage.getItem("dailyGoal");
-  const reachedToday = JSON.parse(localStorage.getItem("reachedToday") || "0");
+  const dailyGoal = Number(localStorage.getItem("dailyGoal")) || Infinity;
+  const reachedToday = JSON.parse(localStorage.getItem("reachedToday") || "{}");
 
   return (
     <Box className={"w-screen flex-none flex flex-col items-center"}>
       <Box className="w-full max-w-7xl px-4 mb-16">
         <Box className="mt-20">
-          <Typography sx={{ fontWeight: 600 }} variant="h3" component={"h1"}>Hi, <span className="bg-gradient-to-r from-green-700 to-yellow-500 text-transparent bg-clip-text">{localStorage.getItem("nickname")}</span><br />{(Number(dailyGoal) || 0) - reachedToday.calories} more calories to reach your daily goal</Typography>
+          <Typography sx={{ fontWeight: 600 }} variant="h3" component={"h1"}>Hi, <span className="bg-gradient-to-r from-green-700 to-yellow-500 text-transparent bg-clip-text">{localStorage.getItem("nickname") || "user"}</span><br />{(dailyGoal) - (reachedToday.calories || 0)} more calories to reach your daily goal</Typography>
         </Box>
         <Box className="flex flex-row flex-wrap mt-20 gap-4">
           <Card>
@@ -21,13 +21,13 @@ const Home = () => {
               <Box className="flex flex-row items-start justify-center gap-12">
                 <Box className="flex flex-col gap-16">
                   <Box className="flex items-center justify-center w-max"><Whatshot color="primary" className="pb-0.5"/><Typography>Calories</Typography></Box>
-                  <Typography variant="h5" component={"p"}>{reachedToday.calories || 0} / {dailyGoal || 0}</Typography>
+                  <Typography variant="h5" component={"p"}>{reachedToday.calories || 0} / {dailyGoal}</Typography>
                 </Box>
                 <Box className="w-32">
                   <CircularProgressbar 
                     value={reachedToday.calories || 0} 
-                    maxValue={dailyGoal ? Number(dailyGoal) : 0} 
-                    text={`${Math.round(((reachedToday.calories || 0) / (dailyGoal ? Number(dailyGoal) : 1)) * 100)}%`}
+                    maxValue={dailyGoal} 
+                    text={`${Math.round(((reachedToday.calories || 0) / (dailyGoal)) * 100)}%`}
                     styles={buildStyles({
                       pathColor: 'rgba(34, 197, 94)',
                       textColor: '#000',
