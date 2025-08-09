1401da16a7dd089939a6921cfc25be32d0289fe5
p3nc1l <szocsdavid598@gmail.com>
2025-08-09 21:38:16 +0300

    added daily goal editor to profile page

--- src/components/pages/Profile.tsx
+++ src/components/pages/Profile.tsx
@@ -7,15 +7,15 @@ const NicknameBox = () => {
   const savedNickname = useContext(SavedDataContext).nickname;
   const setSavedNickname = useContext(SavedDataContext).setNickname;
   const [nickname, setNickname] = useState(savedNickname);
-  const [editingNickname, setEditingNickname] = useState(false);
+  const [editing, setEditing] = useState(false);
 
   const SaveNickname = () => {
     setSavedNickname(nickname);
-    setEditingNickname(false);
+    setEditing(false);
   }
 
-  if (!editingNickname) return (
-    <Box className="w-max cursor-pointer" onClick={() => setEditingNickname(true)}><Box className="text-4xl flex justify-center items-center gap-2">{nickname} <Edit /></Box></Box>
+  if (!editing) return (
+    <Box className="w-max cursor-pointer" onClick={() => setEditing(true)}><Box className="text-4xl flex justify-center items-center gap-2">{nickname} <Edit /></Box></Box>
   )
   else return (
     <Box className="w-max flex"><Box className="w-64"><TextField fullWidth slotProps={{htmlInput: {sx: {fontSize: 36, textAlign: "center"}}}} variant="standard" value={nickname} onChange={e => setNickname(e.target.value)} /></Box><IconButton onClick={SaveNickname} disabled={nickname.length < 3 || nickname.length > 20}><Done /></IconButton></Box>
@@ -54,6 +54,37 @@ const ResetDataDialog = ({ visible = true, closeDialog }: { visible?: boolean, c
   )
 }
 
+const DailyGoalArea = () => {
+  const [editing, setEditing] = useState(false);
+  const savedData = useContext(SavedDataContext);
+  const [dailyGoal, setDailyGoal] = useState(savedData.dailyGoal);
+
+  const SaveChanges = () => {
+    setEditing(false);
+    savedData.setDailyGoal(dailyGoal);
+  }
+
+  if (editing) return (
+    <Box className="flex items-center justify-center w-max">
+      <Box className="flex items-center justify-center gap-2">
+        <Box className="text-xl">Daily Goal:</Box>
+        <Box className="w-20"><TextField fullWidth slotProps={{htmlInput: {type: "number", sx: {fontSize: 20}}}} variant="standard" value={dailyGoal} onChange={e => setDailyGoal(+e.target.value)} /></Box>
+        <IconButton disabled={dailyGoal < 0 || dailyGoal > 100000} onClick={SaveChanges}><Done /></IconButton>
+      </Box>
+    </Box>
+  );
+  else return (
+    <Box className="flex items-center justify-center w-max">
+      <Box>
+        <span className="text-xl">Daily Goal: {dailyGoal}</span>
+      </Box>
+      <Box>
+        <IconButton onClick={() => setEditing(true)}><Edit /></IconButton>
+      </Box>
+    </Box>
+  )
+}
+
 const Profile = () => {
   const [resetDialog, setResetDialog] = useState(false);
 
@@ -65,6 +96,7 @@ const Profile = () => {
     <>
       <ResetDataDialog visible={resetDialog} closeDialog={CloseResetDialog} />
       <Box className="mt-16 mx-auto w-max"><AvatarArea /></Box>
+      <Box className="mt-16"><DailyGoalArea /></Box>
       <Box className="w-max mx-auto mt-8"><Button variant="outlined" color="error" size="large" onClick={() => setResetDialog(true)}>Reset Data</Button></Box>
     </>
   )
